c42a93e32c057e4f37dd13046ee01f573e2f84b6
Jason Ng <S10262552@connect.np.edu.sg>
2024-04-21 19:50:52 +0800

    Practical 1 Complete

--- Practical_1/WritingFile.js
+++ Practical_1/WritingFile.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 
 const content = 'Some content!';
 
-fs.writeFile('/Users/joe/text.txt', content, err => {
+fs.writeFile('C:/Users/zekke/Desktop/BED/BED2024Apr_P03_S10262552/Practical_1/Hi.txt', content, err => {
     if (err) {
         console.error(err);
         return;
